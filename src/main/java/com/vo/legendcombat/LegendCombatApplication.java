3c06b354080b8734dc67326a081144be34b55314
Valentyn Opanasiuk <opanasyuk.valentin@gmail.com>
2025-09-03 21:00:52 -0700

    Init commit

---
+++ src/main/java/com/vo/legendcombat/LegendCombatApplication.java
@@ -0,0 +1,13 @@
+package com.vo.legendcombat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LegendCombatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LegendCombatApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/vo/legendcombat/LegendCombatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vo.legendcombat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LegendCombatApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
